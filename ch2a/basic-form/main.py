62ba56a768fd35a68a7399c9b6612aa3f1d3e1af
john doe <scottbruceburton@gmail.com>
2017-02-08 22:36:12 -0500

    finished ch2a - templates

--- ch2a/basic-form/main.py
+++ ch2a/basic-form/main.py
@@ -1,9 +1,33 @@
+import os
 import webapp2
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.write(*a, **kw)
+
+    def render_str(self, template, **kw):
+        t = jinja_env.get_template(template)
+        return t.render(kw)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class MainPage(Handler):
     def get(self):
-        self.response.write('<p style="font-size:200%">Hello Launchcode!!</p>')
+        items = self.request.get_all("food")
+        self.render("shopping_list.html", items = items)
+
+class FizzBuzzHandler(Handler):
+    def get(self):
+        n = self.request.get("n", 0)
+        n = n and int(n)
+        self.render('fizzbuzz.html', n=n)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPage),
+    ('/fizzbuzz', FizzBuzzHandler)
 ], debug=True)
